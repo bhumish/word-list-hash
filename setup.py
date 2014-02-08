ead1297264e3c6ef511af90313f6456a5ce28259
Bhumish <bhumish@users.noreply.github.com>
2014-02-08 14:48:16 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name		      = 'wordlisthash',
+	version		    = '1.0.0',
+	description	  = 'A handy tool for creating hashes out of given passwords as string or file.',
+	py_modules	  = ['wordlisthash'],
+	author		    = 'Bhumish Gajjar',
+	author_email	= 'bhumish.gajjar@gmail.com',
+	url		        = 'http://bhumishgajjar.wordpress.com',
+	keywords	    = 'md5 hash',
+)
+	
